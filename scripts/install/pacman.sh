268270573ae333054bc82b02663353ce66e60041
chun <lamchunyu00@gmail.com>
2021-01-21 04:41:29 +0000

    Initialized pacman.sh

---
+++ scripts/install/pacman.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# pacman.sh - Installed additional packages after arch-chrooted into the arch installation
+# This script takes two arguments, the hostname and the locale to be installed
+# e.g. pacman.sh alan en_US.UTF-8
+
+host=$1
+locale=$2
+
+# Setting up of swapfile
+truncate -s 0 /swap/swapfile
+chattr +C /swap/swapfile
+btrfs property set /swap/swapfile compression none
+dd if=/dev/zero of=/swap/swapfile bs=1G count=8 status=progress
+chmod 600 /swap/swapfile
+mkswap /swap/swapfile
+swapon /swap/swapfile
+
+
