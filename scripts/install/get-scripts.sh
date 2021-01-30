50340de0277bb0891407cd468d6c4c35d83b2361
chun <lamchunyu00@gmail.com>
2021-01-30 07:29:17 +0000

    fixed typo

--- scripts/install/get-scripts.sh
+++ scripts/install/get-scripts.sh
@@ -3,12 +3,12 @@
 # get-script.sh  - gets the different scripts via curl to its respective places for proper arch installation
 
 echo "Getting scripts for Arch installation..."
-curl https:raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/btrfs.sh -o btrfs.sh
-curl https:raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/initial.sh -o initial.sh
-curl https:raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/pacstrap.sh -o pacstrap.sh
-curl https:raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/swap.sh -o swap.sh
-curl https:raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/pacman.sh -o pacman.sh
-curl https:raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/install.sh -o isntall.sh
+curl https://raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/btrfs.sh -o btrfs.sh
+curl https://raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/initial.sh -o initial.sh
+curl https://raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/pacstrap.sh -o pacstrap.sh
+curl https://raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/swap.sh -o swap.sh
+curl https://raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/pacman.sh -o pacman.sh
+curl https://raw.githubusercontent.com/gunbux/dotfiles/testing/scripts/install/install.sh -o isntall.sh
 
 # Change permisions for all shell scripts
 chmod 777 btrfs.sh
